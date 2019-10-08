d341ef40c148c51c80b0c813ab18706fb8826a68
Felix Castro <felixcastro1993@gmail.com>
2019-10-08 17:02:59 -0500

    metod insercion proporiconado por texto

--- metOrdeInse01.java
+++ metOrdeInse01.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaClassVec;
+package vec;
 
 import java.util.Scanner;
 import javax.swing.JOptionPane;
@@ -16,24 +16,41 @@ public class metOrdeInse01 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         
-        int vector [];
-        int nElem;
+        int [] vector;
+        int nElem, aux;
         
         nElem=Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero "
                 + "de elementos al vector"));
         
         vector=new int[nElem];
         
+        //llenar vector
         for (int i = 0; i < nElem; i++) {
             System.out.println("Ingresa el numero "+(i+1));
             vector[i]=entrada.nextInt();
         }
         
-        for (int i = 1; i < nElem-1; i++) {
-            int j=i-1;
-            while (j>=0 && vector[j]>vector[j+1]) {                
-                
+        //metodo insercion
+        for (int i = 1; i < nElem; i++) {
+            //buscamos la posicion correecta del elemento destino
+            int j=i;
+            aux = vector[i];
+            //localizamos el punto de insercion buscando hacia abajo
+            while (j>0 && aux<vector[j-1]) {                
+                vector[j] = vector[j-1];
+                j--;
             }
+            vector[j] = aux;
+        }
+        
+        System.out.println("Vector en orden ascendente");
+        for (int i = 0; i < nElem; i++) {
+            System.out.println(vector[i]);
+        }
+        
+        System.out.println("Vector en orden descendente");
+        for (int i = (nElem-1); i >= 0; i--) {
+            System.out.println(vector[i]);
         }
     }
 }
